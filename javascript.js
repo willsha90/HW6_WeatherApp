3ef6e40268b8a046a422ef7685ef3e44712363d4
Will <shaheenwill@gmail.com>
2021-04-26 20:49:06 -0400

    updated js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,41 @@
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=dbf5f0cf87ab9e0799c0fb6cd0b54ea4')
-  .then(response => response.json())
-  .then(data => console.log(data));
+// javascript.js
+console.log('Aloha')
 
-//   http:api.openweathermap.org/data/2.5/forecast?q=Boston&appid=
-//   dbf5f0cf87ab9e0799c0fb6cd0b54ea4'
-  // http://api.openweathermap.org/data/2.5/forecast?q=Boston&appid=dbf5f0cf87ab9e0799c0fb6cd0b54ea4'
+// **** GLOBAL DECLARATIONS*********
+const baseURI = 'http://api.openweathermap.org/data/2.5/';
+const myAPIId = 'dbf5f0cf87ab9e0799c0fb6cd0b54ea4';
+const btnSubmit = document.querySelector('.btn-user-city');
+const userCity = document.querySelector('#city-search');
 
-  fetch('http://api.openweathermap.org/data/2.5/forecast?q=Boston&appid=dbf5f0cf87ab9e0799c0fb6cd0b54ea4')
-  .then(response => response.json())
-  .then(data => console.log(data));
 
+// **** FUNCTION DEFINITIONS*********
+function getCityWeatherData(myCity, myForecastType) {
+  var myFetchString = baseURI + myForecastType + '?q=' + myCity + '&appid=' + myAPIId;
+
+  
+  // fetch('http://api.openweathermap.org/data/2.5/weather?q=Boston&appid=dbf5f0cf87ab9e0799c0fb6cd0b54ea4')
+  
+ fetch(myFetchString)
+    .then(function(response)  {return response.json()})
+    .then(function(data)  {return data});
+}
+
+function displayData() {
+console.log('in displayData');
+}
+
+
+function handleClick(e) {
+  var myCity = userCity.value
+  var myCityData = getCityWeatherData(myCity, 'weather')
+  console.log(myCityData);
+  
+  displayData()
+  // getCityWeatherData(myCity, 'forecast')
+  
+}
+
+
+//*****ON PAGE LOAD  *************
+
+btnSubmit.addEventListener("click", handleClick)
